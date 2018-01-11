42145a7c83012816d5dda5fc41a6239d5d0ce0b9
Tom <thomas.le-morvan@grenoble-inp.fr>
2018-01-11 17:02:12 +0100

    API better

--- parser.py
+++ parser.py
@@ -1,10 +1,11 @@
-MyFile = open("C:\Users\ensimag\Documents\ProjetMaya\Lowres.off","r")
+import time
+tmps1=time.clock()
+MyFile = open("C:\Users\ensimag\Documents\MayaProject\Lowres.off","r")
 data = MyFile.readline().rstrip('\n\r').split(" ")
 if data[0] != "OFF" :
     print("Error : il faut lire un fichier OFF")
 else :
     data = MyFile.readline().rstrip('\n\r').split(" ")
-    print(data)
     nb_vertices, nb_faces = int(data[0]),int(data[1])
     Vertices = []
     for i in range(0,nb_vertices) :
@@ -22,3 +23,5 @@ else :
 cmds.select(all=True)
 cmds.polyUnite()
 MyFile.close()
+tmps2=time.clock()
+print "Temps d'execution = %d\n" %(tmps2-tmps1)
--- parserAPI.py
+++ parserAPI.py
@@ -1,4 +1,6 @@
 import maya.api.OpenMaya as OM
+import time
+tmps1=time.clock()
 MyFile = open("C:\Users\ensimag\Documents\MayaProject\Lowres.off","r")
 mesh = OM.MFnMesh()
 data = MyFile.readline().rstrip('\n\r').split(" ")
@@ -6,7 +8,6 @@ if data[0] != "OFF" :
     print("Error : il faut lire un fichier OFF")
 else :
     data = MyFile.readline().rstrip('\n\r').split(" ")
-    print(data)
     nb_vertices, nb_faces = int(data[0]),int(data[1])
     Vertices = []
     for i in range(0,nb_vertices) :
@@ -20,6 +21,7 @@ else :
         points = []
         for j in range(vert) :
             points.append(OM.MPoint(Vertices[3*int(data[j+1])],Vertices[3*int(data[j+1])+1],Vertices[3*int(data[j+1])+2]))
-        print(points)
         mesh.addPolygon(points, mergeVertices=True, pointTolerance=0.0001)
 MyFile.close()
+tmps2=time.clock()
+print "Temps d'execution = %d\n" %(tmps2-tmps1)
