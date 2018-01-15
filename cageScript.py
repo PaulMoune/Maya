87aa656fb97908144290f74a30f6baf03e32b6f0
PaulMoune <paul.moune@gmail.com>
2018-01-15 16:10:04 +0100

    script ok

--- cageScript.py
+++ cageScript.py
@@ -3,5 +3,11 @@ import maya.cmds as cmds;
 cmds.duplicate('polySurface1');
 cmds.select('polySurface2');
 cmds.polyReduce(p=80,ch=False,kev=True);
-cmds.select('polySurface2.vtx[0:196]');
-#cmds.moveVertexAlongDirection(n = 0.367418);
+cmds.select('polySurface2');
+nor = [];
+nb = cmds.polyEvaluate(v=True);
+for i in range(nb):
+    nor.append(0.367418);
+print(nor)
+cmds.select('polySurface2.vtx[:]');
+cmds.moveVertexAlongDirection(n = nor);
